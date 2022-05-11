179baab4d12f03a016ef6d2fc8e6590e29105d3f
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2022-05-10 21:45:18 -0400

    Updating for npm audit, removing unused jest, adding in PR GHA file

--- src/templates/javascript/markup.jsx
+++ src/templates/javascript/markup.jsx
@@ -4,4 +4,4 @@ import styles from "./LOWER_NAME.module.scss";
 
 const UPPER_NAME = () => <div>UPPER_NAME</div>;
 
-export default UPPER_NAME;
+export { UPPER_NAME };
--- src/templates/typescript/markup.tsx
+++ src/templates/typescript/markup.tsx
@@ -4,4 +4,4 @@ import styles from "./LOWER_NAME.module.scss";
 
 const UPPER_NAME = (): JSX.Element => <div>UPPER_NAME</div>;
 
-export default UPPER_NAME;
+export { UPPER_NAME };
